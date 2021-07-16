f4b76a54912b84ce5625eac39fd283c032f533f5
Koulinn <drdverzola@gmail.com>
2021-07-16 08:50:22 +0100

    Route are defined

---
+++ spotify/src/Components/AlbumPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AlbumPage() {
+    return (
+        <div>
+            Album Page
+        </div>
+    )
+}
+
+export default AlbumPage
---
+++ spotify/src/Components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MainPage from './MainPage'
+import TopBar from './TopBar'
+import PlayerBottom from './PlayerBottom'
+import AlbumPage from './AlbumPage'
+import ArtistPage from './ArtistPage'
+
+export default function Home() {
+    return (
+        <Container id="allContentWrapper" className="p-0" fluid>
+            <Router>
+                <TopBar></TopBar>
+        
+                <Route path="/" exact render={(routerProps)=> <MainPage {...routerProps}></MainPage>}></Route>
+                <Route path="/albumPage" exact render={(routerProps)=> <AlbumPage {...routerProps}></AlbumPage>}></Route>
+                <Route path="/artistPage" exact render={(routerProps)=> <ArtistPage {...routerProps}></ArtistPage>}></Route>
+
+                <PlayerBottom></PlayerBottom>
+            </Router>
+            
+        </Container >
+    );
+}
