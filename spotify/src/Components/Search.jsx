a3253c15a3c090b7c13813945d466da393be382f
Koulinn <drdverzola@gmail.com>
2021-07-17 08:54:08 +0100

    imported TopBar to Search Comp

--- spotify/src/Components/Search.jsx
+++ spotify/src/Components/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import TopBar from './TopBarComp/TopBar'
 
 function Search() {
     return (
-        <div>
-            Hello I'm Search comp
-        </div>
+        <main className="container-fluid p-0 m-0">
+            <TopBar></TopBar>
+
+        </main>
     )
 }
 
