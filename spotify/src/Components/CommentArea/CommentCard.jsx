deb8cd9baf0d9152a18dd4f70c2edf289df517d0
Koulinn <drdverzola@gmail.com>
2021-07-18 13:26:59 +0100

    Comment Card Bottom Stats styling done

--- spotify/src/Components/CommentArea/CommentCard.jsx
+++ spotify/src/Components/CommentArea/CommentCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CommentCardBottomStats from './CommentCardBottomStats'
 import CommentCardTopMetaStats from './CommentCardTopMetaStats'
 import CommentTextArea from './CommentTextArea'
 
@@ -7,6 +8,7 @@ function CommentCard() {
         <div className="card-Wrapper">
             <CommentCardTopMetaStats></CommentCardTopMetaStats>
             <CommentTextArea></CommentTextArea>
+            <CommentCardBottomStats></CommentCardBottomStats>
             
         </div>
     )
